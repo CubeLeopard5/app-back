cf87def2cc28bf1c1996d5a428401b505fb9c867
Adrien Michaud <adrien.michaud@epitech.eu>
2023-06-29 18:12:17 +0200

    add middelware to router express

--- src/routes/birds.js
+++ src/routes/birds.js
@@ -1,16 +1,17 @@
 const express = require('express')
 const router = express.Router()
+const auth = require("../middelwares/auth");
+const cors = require('cors');
+router.use(cors());
 
-// middleware that is specific to this router
 router.use((req, res, next) => {
-    console.log('Time: ', Date.now())
-    next()
+	auth(req, res, next);
 })
-// define the home page route
+
 router.get('/', (req, res) => {
-    res.send('Birds home page')
+    res.status(200).send('Birds home page')
 })
-// define the about route
+
 router.get('/about', (req, res) => {
     res.send('About birds')
 })
